1fedfbe285362dce490e7cf950d969201f4e4b44
newmersedez <volkovdm333@gmail.com>
2026-01-13 23:58:58 +0300

    Completed 12

--- quests/12.enum/solution.go
+++ quests/12.enum/solution.go
@@ -1,10 +1,12 @@
 package enum
 
+import "fmt"
+
 type OrderState int
 
 const (
 	StateCreated OrderState = iota
-	StatePaid
+	StatePaid 
 	StatePacked
 	StateShipped
 	StateDelivered
@@ -13,11 +15,63 @@ const (
 )
 
 func (s OrderState) String() string {
-	// TODO: implement
-	return ""
+	var states = map[OrderState]string{
+		StateCreated:   "created",
+		StatePaid:      "paid",
+		StatePacked:    "packed",
+		StateShipped:   "shipped",
+		StateDelivered: "delivered",
+		StateCancelled: "cancelled",
+		StateReturned:  "returned",
+	}
+
+	return states[s]
 }
 
 func NextState(current OrderState, action string) OrderState {
-	// TODO: implement
-	return current
+	switch current {
+	case StateCreated:
+		switch action {
+		case "pay":
+			return StatePaid
+		case "cancel":
+			return StateCancelled
+		}
+		return current
+
+	case StatePaid:
+		switch action {
+		case "pack":
+			return StatePacked
+		case "cancel":
+			return StateCancelled
+		}
+		return current
+
+	case StatePacked:
+		if action == "ship" {
+			return StateShipped
+		}
+		return current
+
+	case StateShipped:
+		switch action {
+		case "deliver":
+			return StateDelivered
+		case "return":
+			return StateReturned
+		}
+		return current
+
+	case StateDelivered:
+		if action == "return" {
+			return StateReturned
+		}
+		return current
+
+	case StateCancelled, StateReturned:
+		return current
+	default:
+		panic(fmt.Errorf("unknown state: %s", current))
+	}
 }

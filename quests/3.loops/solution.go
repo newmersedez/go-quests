c93b735cd439577670831989d5bdcf166fc20806
newmersedez <volkovdm333@gmail.com>
2026-01-13 16:52:35 +0300

    completed 3

--- quests/3.loops/solution.go
+++ quests/3.loops/solution.go
@@ -1,14 +1,45 @@
 package loops
 
 func SumEvenNumbers(n int) int {
-	// TODO: implement
-	// Read README.md for the instructions
-	return 0
+	res := 0
+
+	if n <= 0 {
+		return res
+	}
+
+	for number := range (n + 1) {
+		if number % 2 == 0 {
+			res += number
+		}
+	}
+
+	return res
 }
+
 
 func KeepOnlyConsonants(strs []string) []string {
 	res := []string{}
-	// TODO: implement
-	// Read README.md for the instructions
+	
+	if len(strs) == 0 {
+		return res
+	}
+
+	for _, str := range strs {
+		newStr := make([]rune, 0, len(str))
+
+		for _, ch := range str {
+			switch ch {
+			case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+				continue
+			default:
+				newStr = append(newStr, ch)
+			}
+		}
+
+		if len(newStr) > 0 {
+			res = append(res, string(newStr))
+		}
+ 	}
+
 	return res
 }

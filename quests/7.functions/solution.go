0e465109c859a533f678a10685e2f289836c7302
newmersedez <volkovdm333@gmail.com>
2026-01-13 18:01:58 +0300

    completed 7

--- quests/7.functions/solution.go
+++ quests/7.functions/solution.go
@@ -1,30 +1,52 @@
 package functions
 
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
 // Divide returns a / b or an error if b == 0
 func Divide(a, b int) (int, error) {
-	// TODO: implement
-	// Read README.md for instructions on returning proper errors
-	return 0, nil
+	if b == 0 {
+		return 0, fmt.Errorf("b cannot be zero")
+	}
+
+	return a / b, nil
 }
 
 // SumAll returns the sum of all provided integers
 func SumAll(nums ...int) int {
-	// TODO: implement
-	// Read README.md for instructions on handling variadic arguments
-	return 0
+	res := 0
+	for _, number := range nums {
+		res += number
+	}
+
+	return res
 }
 
 // MaxMin returns the max and min of all provided integers
 // Returns an error if no numbers are provided
 func MaxMin(nums ...int) (int, int, error) {
-	// TODO: implement
-	// Read README.md for instructions on proper error handling
-	return 0, 0, nil
+	if len(nums) == 0 {
+		return 0, 0, fmt.Errorf("nums is empty")
+	}
+
+	min := math.MaxInt64
+	max := math.MinInt64
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+		if num < min {
+			min = num
+		}
+	}
+
+	return max, min, nil
 }
 
 // ConcatAll joins all strings using the provided separator
 func ConcatAll(sep string, strs ...string) string {
-	// TODO: implement
-	// Read README.md for instructions on variadic strings
-	return ""
+	return strings.Join(strs, sep)
 }

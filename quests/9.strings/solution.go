0fb459a2cbe26c707323c3a1379cee2750ab0ab9
newmersedez <volkovdm333@gmail.com>
2026-01-13 23:19:20 +0300

    Completed 9

--- quests/9.strings/solution.go
+++ quests/9.strings/solution.go
@@ -6,31 +6,41 @@ type TextStats struct {
 }
 
 func AnalyzeText(s string) TextStats {
-	// TODO: implement
-	// Read README.md for the instructions
-	return TextStats{}
+	return TextStats{
+		ByteLength: len([]byte(s)),
+		RuneCount: len([]rune(s)),
+	}
 }
 
 func RuneFrequencies(s string) map[rune]int {
-	// TODO: implement
-	// Read README.md for the instructions
-	return map[rune]int{}
+	runes := make(map[rune]int)
+	for _, ch := range s {
+		if _, exists := runes[ch]; !exists {
+			runes[ch] = 0
+		}
+		runes[ch]++;
+	}
+	return runes
 }
 
 func FirstRunePosition(s string, target rune) int {
-	// TODO: implement
-	// Read README.md for the instructions
+	for i, ch := range s {
+		if ch == target {
+			return i
+		}
+	}
 	return -1
 }
 
 func ExtractRunes(s string) []rune {
-	// TODO: implement
-	// Read README.md for the instructions
-	return []rune{}
+	return []rune(s)
 }
 
 func HasOnlyASCII(s string) bool {
-	// TODO: implement
-	// Read README.md for the instructions
-	return false
+	for _, ch := range s {
+		if ch > 127 {
+			return false
+		}
+	}
+	return true
 }

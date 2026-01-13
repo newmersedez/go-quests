0243040040f7f318412529d22679ebb057f3bc5b
newmersedez <volkovdm333@gmail.com>
2026-01-13 17:22:12 +0300

    completed 5

--- quests/5.slice/solution.go
+++ quests/5.slice/solution.go
@@ -1,7 +1,22 @@
 package slice
 
 func ProcessScores(scores []int) []int {
-	//TODO: implement
-	// Read README.md for the instructions
-	return []int{}
+	res := make([]int, 0, len(scores))
+
+	for _, score := range scores {
+		if score < 0 || score > 100 {
+			continue
+		} else if score < 40 {
+			score = 40
+		}
+		res = append(res, score)
+	}
+
+	if len(res) > 5 {
+		for i, score := range res {
+			res[i] = min(score + 5, 100)
+		}
+	}
+
+	return res
 }

9f45526c6e098dcd8d69615204fc4172552c5727
newmersedez <volkovdm333@gmail.com>
2026-01-14 15:24:31 +0300

    Completed 14

--- quests/14.error/solution.go
+++ quests/14.error/solution.go
@@ -1,9 +1,15 @@
 package error
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
 // Part 1: Define sentinel errors
 var (
-// TODO: Define ErrEmptyFilename
-// TODO: Define ErrFileTooLarge
+	ErrEmptyFilename = errors.New("filename cannot be empty")
+	ErrFileTooLarge = errors.New("file size exceeds limit")
 )
 
 // Part 2: Define custom error type
@@ -14,34 +20,60 @@ type ValidationError struct {
 
 // TODO: Implement Error() method
 func (ve *ValidationError) Error() string {
-	return ""
+	return fmt.Sprintf("validation failed for %s: %s", ve.Filename, ve.Reason)
 }
 
 // Part 3: Validation functions
 
 func ValidateFilename(filename string) error {
-	// TODO: Implement
+	if filename == "" {
+		return ErrEmptyFilename
+	}
+
 	return nil
 }
 
 func ValidateFileSize(size int64, maxSize int64) error {
-	// TODO: Implement
+	if size > maxSize {
+		return ErrFileTooLarge
+	}
+
+	if size < 0 {
+		return errors.New("file size cannot be negative")
+	}
+
 	return nil
 }
 
 func ValidateFileExtension(filename string, allowedExts []string) error {
-	// TODO: Implement
-	return nil
+	for _, ext := range allowedExts {
+		if strings.HasSuffix(filename, ext) {
+			return nil
+		}
+	}
+	return &ValidationError{
+		Filename: filename,
+		Reason: "unsupported file extension",
+	}
 }
 
 func ValidateFile(filename string, size int64, maxSize int64) error {
-	// TODO: Implement
+	if err := ValidateFilename(filename); err != nil {
+		return fmt.Errorf("file validation failed: %w", err)
+	}
+	if err := ValidateFileSize(size, maxSize); err != nil {
+		return fmt.Errorf("file validation failed: %w", err)
+	}
 	return nil
 }
 
-// Part 4: Error checking
-
 func CanRetry(err error) bool {
-	// TODO: Implement
+	var verr *ValidationError;
+	if errors.As(err, &verr) {
+		return true
+	}
+	if errors.Is(err, ErrEmptyFilename) || errors.Is(err, ErrFileTooLarge) {
+		return true
+	}
 	return false
 }

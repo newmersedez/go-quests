d86f935c9b02b5b7dd5ee9383304edd521bfe99b
newmersedez <volkovdm333@gmail.com>
2026-01-14 15:47:42 +0300

    Completed 16

--- quests/16.channel/solution.go
+++ quests/16.channel/solution.go
@@ -1,19 +1,28 @@
 package channel
 
+import "fmt"
+
 // TODO:Implement ReadFromBoth function
 // Read README.md for the instructions
 func ReadFromBoth(ch1 chan string, ch2 chan string) string {
-	return ""
+	val1 := <- ch1
+	val2 := <- ch2
+	return fmt.Sprintf("read: %s & %s", val1, val2)
 }
 
 // TODO:Implement ReadFromBoth function
 // Read README.md for the instructions
 func WriteToBoth(ch1 chan string, ch2 chan string, msg string) {
+	go func () {
+		ch1 <- fmt.Sprintf("write: %s", msg)
+	}()
 
+	go func() {
+		ch2 <- fmt.Sprintf("write: %s", msg)
+	}()
 }
 
-// TODO:Implement ReadFromBoth function
-// Read README.md for the instructions
 func ReadThenWrite(input chan string, output chan string) {
-
+	val := <- input
+	output <- fmt.Sprintf("transform: %s", val)
 }

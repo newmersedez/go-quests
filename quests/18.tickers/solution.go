69358fc6bf3e5e3efaeefbc50af0b857c14d7cd2
newmersedez <volkovdm333@gmail.com>
2026-01-14 16:20:55 +0300

    Completed 18

--- quests/18.tickers/solution.go
+++ quests/18.tickers/solution.go
@@ -1,7 +1,28 @@
 package tickers
 
+import (
+	"fmt"
+	"time"
+)
+
 func Ticker() {
-	// TODO:Implement Ticker function
-	// Read README.md for the instructions
+	helloTicker := time.NewTicker(500 * time.Millisecond)
+	defer helloTicker.Stop()
+	
+	worldTicker := time.NewTicker(1000 * time.Millisecond)
+	defer worldTicker.Stop()
+	
+	done := time.After(3 * time.Second)
+	
+	for {
+		select{
+		case <- done:
+			return
+		case <- helloTicker.C:
+			fmt.Println("hello")
+		case <- worldTicker.C:
+			fmt.Println("world")
+		}	
+	}
 
 }
